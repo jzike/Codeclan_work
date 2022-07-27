28c0c70ec49bfe97bfc143c3358f28967d44ffe1
jzike <107878458+jzike@users.noreply.github.com>
2022-07-27 10:11:55 +0100

    Update first_queries.sql

--- Week_03/Day_1/first_queries.sql
+++ Week_03/Day_1/first_queries.sql
@@ -8,6 +8,7 @@
 -- Get me a table of all the animals information
 
 -- SELECT = columns to select
+-- * = all the columns
 -- FROM = table / entity to select FROM 
 -- ; = end of query
 
@@ -23,10 +24,13 @@ FROM animals
 WHERE id = 2;
 
 -- Task: Get me a table of information about Ernest the Snake
+-- Hint: in sql, varchars are referenced with 'single quotes'
 
 SELECT *
 FROM animals 
 WHERE id = 7;
+
+-- Because animal ID is a primary key, we can expect only one result from the above query
 
 SELECT *
 FROM animals 
